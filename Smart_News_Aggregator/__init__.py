64922286428554aa02c63dc314933cd340513dca
vidhikamangla <vidhikamangla@gmail.com>
2025-11-18 17:41:32 +0530

    Updated MCP Server

--- Smart_News_Aggregator/__init__.py
+++ Smart_News_Aggregator/__init__.py
@@ -1,1 +1,21 @@
 from . import agent_old
+# Original agent
+# from .agent import root_agent
+
+#Enhanced agent with MCP & A2A
+from .agent import root_agent as enhanced_root_agent
+from .agent  import get__agent_async
+
+# NEW: MCP server (for external exposure)
+from . import mcp_server
+
+# NEW: CrewAI bridge
+from . import crewai_bridge_agent
+
+__all__ = [
+    'root_agent',              # Original
+    'enhanced_root_agent',     # Enhanced version
+    'get_agent_async', # MCP consumer
+    'mcp_server',
+    'crewai_bridge_agent'
+]
